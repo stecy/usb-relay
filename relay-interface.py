b98d728b3414d856bcfb7798f4985465aa0ee222
Stecy Gravel <stecy.gravel@gmail.com>
2019-01-05 14:24:32 -0500

    Correcting issues

--- relay-interface.py
+++ relay-interface.py
@@ -1,15 +1,18 @@
+#!/usr/bin/env python
+
 import serial
 import argparse
 
 parser = argparse.ArgumentParser(prog='relay-interface', description='Control a relay over USB')
-parser.add_argument('usb', type=int, help='path')
+parser.add_argument('port', help='Port to use.  Example: /dev/ttyUSB1 or COM1')
 parser.add_argument('switch', type=int, help='1 to turn ON and 0 to turn OFF')
 args = parser.parse_args()
 
-device = serial.Serial('/dev/ttyUSB1', 9600)
+device = serial.Serial(args.port, 9600)
 
 
-if args.switch == '1':
-	device.write('1')
+if args.switch == 1:
+	device.write(1)
+	print('on')
 else:
-	device.write('0')
+	device.write(0)
