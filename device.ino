ab7a09b07113390156bb858009771af91b3fc769
Stecy Gravel <stecy.gravel@gmail.com>
2019-01-06 17:32:03 -0500

    Signed-off-by: Stecy Gravel <stecy.gravel@gmail.com>

--- device.ino
+++ device.ino
@@ -1,4 +1,4 @@
-#define RELAY 10
+#define RELAY 12
 #define LED 13
 
 void setup() {
@@ -8,15 +8,23 @@ void setup() {
 }
 
 void loop() {
-    if (Serial.available()) {
-        char command = Serial.read();
-        if (command == '1') {
-            digitalWrite(RELAY, HIGH);
-            digitalWrite(LED, HIGH);
-        }
-        else if (command == '0') {
+    if (Serial.available()) 
+    {
+        switch(Serial.read())
+        {
+          case '0':
             digitalWrite(RELAY, LOW);
             digitalWrite(LED, LOW);
+            Serial.println("Off");
+            break;
+          case '1':
+            digitalWrite(RELAY, HIGH);
+            digitalWrite(LED, HIGH);
+            Serial.println("On!");
+            break;
+          default:
+            Serial.println("WTF");
+            break;
         }
     }
 }
--- relay-interface.py
+++ relay-interface.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
 import serial
+import time
 import argparse
 
 parser = argparse.ArgumentParser(prog='relay-interface', description='Control a relay over USB')
 parser.add_argument('port', help='Port to use.  Example: /dev/ttyUSB1 or COM1')
-parser.add_argument('switch', type=int, help='1 to turn ON and 0 to turn OFF')
+parser.add_argument('switch',type=bytes, help='1 to turn ON and 0 to turn OFF')
 args = parser.parse_args()
 
 try:
 	device = serial.Serial(args.port, 9600)
+	time.sleep(2)
 except:
 	print('Please check the port!')
 	exit
-if args.switch == 1:
-	device.write(b'1')
-else:
-	device.write(b'0')
+
+device.write(args.switch)
+
+data = device.read(3)
+print(data)
+
+#device.close()
+
